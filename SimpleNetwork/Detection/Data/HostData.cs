c2a244076750072b93d3506f2473741b4415187c
Zakant <zakant@gmx.de>
2014-07-30 15:54:54 +0200

    Dokumentation für "HostData" hinzugefügt

--- SimpleNetwork/Detection/Data/HostData.cs
+++ SimpleNetwork/Detection/Data/HostData.cs
@@ -8,18 +8,36 @@ using System.Text;
 
 namespace SimpleNetwork.Detection.Data
 {
+    /// <summary>
+    /// Repräsentiert die Verbindungsinformationen eines Hosts.
+    /// </summary>
     [Serializable]
     public class HostData 
     {
+        /// <summary>
+        /// Die ID des Hots.
+        /// </summary>
         public Guid ID { get; set; }
 
+        /// <summary>
+        /// Eine Auflistung aller möglichen IP Adressen des Hosts.
+        /// </summary>
         public IPAddress[] Addresses { get; set; }
 
+        /// <summary>
+        /// Der Port des Hosts.
+        /// </summary>
         public int Port { get; set; }
 
+        /// <summary>
+        /// Der Name des Hosts.
+        /// </summary>
         public string Name { get; set; }
 
-
+        /// <summary>
+        /// Konvertiert diese HostData-Objekt in seine Byte Array Darstellung.
+        /// </summary>
+        /// <returns>Die Byte Array Darstellung diese HostData-Objektes</returns>
         public byte[] toByteArray()
         {
             BinaryFormatter f = new BinaryFormatter();
@@ -28,6 +46,11 @@ namespace SimpleNetwork.Detection.Data
             return ms.GetBuffer();
         }
 
+        /// <summary>
+        /// Konvertiert eine Bytefolge in ein HostData-Objekt.
+        /// </summary>
+        /// <param name="data">Die Bytefolge, aus der das HostData-Objekt erzeugt wird.</param>
+        /// <returns>Das erzeugte HostData-Objekt.</returns>
         public static HostData fromByteArray(byte[] data)
         {
             BinaryFormatter f = new BinaryFormatter();
