d5717a8eb8cbca17bdacdf8ae9bae9b2b5db6f4f
Zakant <zakant@gmx.de>
2014-07-30 16:50:33 +0200

    Dokumentation für "NewMessageEventArgs" hinzugefügt

--- SimpleNetwork/Events/NewMessageEventArgs.cs
+++ SimpleNetwork/Events/NewMessageEventArgs.cs
@@ -7,12 +7,29 @@ using System.Text;
 
 namespace SimpleNetwork.Events
 {
+    /// <summary>
+    /// Stellt Daten für das <see cref="SimpleNetwork.Package.Provider.IPackageProvider.NewMessage"/> Ereigniss bereit.
+    /// </summary>
     public class NewMessageEventArgs : SimpleNetworkEventArgs
     {
+        /// <summary>
+        /// Der Remotehost, von dem das Packet stammt.
+        /// </summary>
         public IClient Client { get; protected set; }
+        /// <summary>
+        /// Das empfangene Packet.
+        /// </summary>
         public IPackage Package { get; protected set; }
+        /// <summary>
+        /// Der Typ des empfangenen Packetes.
+        /// </summary>
         public Type MessageType { get { return (this.Package.GetType()); } }
 
+        /// <summary>
+        /// Initialisiert eine neue Instanz der NewMessageEventArgs unter verwendung des angegeben IClient-Objekts und dem IPackage-Objekt.
+        /// </summary>
+        /// <param name="client">Der IClient, von dem die Nachricht stammt.</param>
+        /// <param name="package">Das empfangene Packet.</param>
         public NewMessageEventArgs(IClient client, IPackage package)
         {
             Client = client;
