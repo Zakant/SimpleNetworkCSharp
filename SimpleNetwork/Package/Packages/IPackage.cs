d130fef1cc5bab5a14ae7b3d6778d19bab1c3509
Zakant <zakant@gmx.de>
2014-07-30 17:42:01 +0200

    Dokumentation für "IPackage" hinzugefügt

---
+++ SimpleNetwork/Package/Packages/IPackage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleNetwork.Package.Packages
+{
+    /// <summary>
+    /// Stellt das Basis Interface für alle zu übertragenden Packete da.
+    /// </summary>
+    public interface IPackage
+    {
+    }
+}
