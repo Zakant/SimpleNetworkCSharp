c3e7c67d73ce37cd7eb1eb4990c8609bfc345fb1
Zakant <zakant@gmx.de>
2014-07-30 17:45:49 +0200

    Zum Repo hinzugefügt

---
+++ SimpleNetwork/Package/Packages/Internal/ShutDownPackage.cs
@@ -0,0 +1,27 @@
+﻿using SimpleNetwork.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleNetwork.Package.Packages.Internal
+{
+    [Serializable]
+    internal class ShutDownPackage : IPackage
+    {
+        public string ShutDownMessage { get; set; }
+        public DisconnectReason Reason { get; set; }
+
+        public ShutDownPackage(string Message)
+        {
+            ShutDownMessage = Message;
+            Reason = DisconnectReason.ClosedProperly;
+        }
+
+        public ShutDownPackage(string Message,DisconnectReason reason)
+        {
+            ShutDownMessage = Message;
+            Reason = reason;
+        }
+    }
+}
