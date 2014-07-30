49024c99a42a2cb0ec0e9eaa5db8c243423c35bf
Zakant <zakant@gmx.de>
2014-07-30 14:43:37 +0200

    Dokumentation zu "IClientFactory" hinzugefügt

--- SimpleNetwork/Client/IClientFactory.cs
+++ SimpleNetwork/Client/IClientFactory.cs
@@ -6,9 +6,22 @@ using System.Text;
 
 namespace SimpleNetwork.Client
 {
+    /// <summary>
+    /// Stellt Methoden bereit, um IClient-Objekte zu erstellen
+    /// </summary>
     public interface IClientFactory
     {
+        /// <summary>
+        /// Erzeugt ein neues IClient-Objekt aus einem angegebenen TcpClient
+        /// </summary>
+        /// <param name="client">Das bei der Erstellung zu verwendene TcpClient-Objekt</param>
+        /// <returns>Das neu erstellte IClient-Objekt</returns>
         IClient createFromTcpClient(TcpClient client);
+        
+        /// <summary>
+        /// Erzeugt ein neues leeres IClient-Objekt
+        /// </summary>
+        /// <returns>Das neue leere IClient-Objekt</returns>
         IClient createDefault();
     }
 }
