5fb6441e9a1ac38c27316cb0ff2bb68c094ccd87
Zakant <zakant@gmx.de>
2014-07-30 17:43:38 +0200

    Dokumentation für "FilePackage" hinzugefügt

---
+++ SimpleNetwork/Package/Packages/FilePackage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SimpleNetwork.Package.Packages
+{
+    /// <summary>
+    /// Stellt ein Packet da, um Datein zu übertragen.
+    /// </summary>
+    [Serializable]
+    public class FilePackage : IPackage
+    {
+        /// <summary>
+        /// Die Bytefolge, durch die die Datei dargestellt.
+        /// </summary>
+        public byte[] Data { get; set; }
+        /// <summary>
+        /// Der Name der Datei.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Das FileInfo-Objekt der Datei.
+        /// </summary>
+        public FileInfo Info { get; set; }
+
+        /// <summary>
+        /// Initialisiert eine neue Instanz der FilePackage-Klasse unter verwendung des angegeben FileInfo-Objekt.
+        /// </summary>
+        /// <param name="fi">Das FileInfo-Objekt.</param>
+        public FilePackage(FileInfo fi)
+        {
+            using (var fs = new FileStream(fi.FullName, FileMode.Open))
+            {
+                Data = (new BinaryReader(fs)).ReadBytes((int)fs.Length);
+            }
+            Name = fi.Name;
+            Info = fi;
+        }
+
+    }
+}
