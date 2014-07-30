4b9c0fab893f54fb21d00b9428bdc9aa19d59ca9
Zakant <zakant@gmx.de>
2014-07-30 15:56:12 +0200

    Dokumentation für "HostDataTime" hinzugefügt

--- SimpleNetwork/Detection/Data/HostDataTime.cs
+++ SimpleNetwork/Detection/Data/HostDataTime.cs
@@ -5,10 +5,19 @@ using System.Text;
 
 namespace SimpleNetwork.Detection.Data
 {
+    /// <summary>
+    /// Stellt ein HostData-Objekt mit einem assozierten TimeStamp da.
+    /// </summary>
     [Serializable]
     public class HostDataTime
     {
+        /// <summary>
+        /// Der TimeStamp.
+        /// </summary>
         public DateTime stamp;
+        /// <summary>
+        /// Das HostData-Objekt.
+        /// </summary>
         public HostData data;
     }
 }
