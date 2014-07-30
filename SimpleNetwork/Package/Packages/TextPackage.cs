947e4d2bc80e59ed7e93894fbda43cfe677e0424
Zakant <zakant@gmx.de>
2014-07-30 17:45:19 +0200

    Dokumentation für "TextPackage" hinzugefügt

---
+++ SimpleNetwork/Package/Packages/TextPackage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleNetwork.Package.Packages
+{
+    /// <summary>
+    /// Stellt ein Packet da, um Text zu übertragen.
+    /// </summary>
+    [Serializable]
+    public class TextPackage : IPackage
+    {
+        /// <summary>
+        /// Der Text.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Initialisert eine leere Instanz der TextPackage-Klasse.
+        /// </summary>
+        public TextPackage() { }
+
+        /// <summary>
+        /// Initialisiert eine neue Instanz der TextPackage-Klasse unter verwendung des angegeben strings.
+        /// </summary>
+        /// <param name="value">Der Text.</param>
+        public TextPackage(string value)
+        {
+            Value = value;
+        }
+    }
+}
