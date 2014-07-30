135b74c62fcc082cafc7bb34ed2e9fc401b97ea7
Zakant <zakant@gmx.de>
2014-07-30 14:40:50 +0200

    Dokumentation für "IClient" hinzugefügt

--- SimpleNetwork/Client/IClient.cs
+++ SimpleNetwork/Client/IClient.cs
@@ -10,22 +10,53 @@ using System.Text;
 
 namespace SimpleNetwork.Client
 {
+    /// <summary>
+    /// Stellt eine Objekt da, dass über das Netzwerk mit einem Remotehost verbunden ist
+    /// </summary>
     public interface IClient : IPackageProvider
     {
+        /// <summary>
+        /// Tritt ein, wenn der Client die Verbindung zum Remotehost verliert
+        /// </summary>
         event EventHandler<SimpleNetwork.Events.DisconnectedEventArgs> Disconnected;
 
+        /// <summary>
+        /// Gibt an, ob das Client-Objekt eine aktive Verbindung besitzt. True fals ja, False, false nein.
+        /// </summary>
         bool isConnected { get; }
 
+        /// <summary>
+        /// Verbindet mit einem Remote Server unter verwendunge der angegebenen IPAdresse und dem Port
+        /// </summary>
+        /// <param name="ip">Die zu verwendene IPAdresse</param>
+        /// <param name="port">Der zu verwendene Port</param>
         void Connect(IPAddress ip, int port);
 
+        /// <summary>
+        /// Verbindet mit einem Remote Server unter verwendung des angegeben HostData-Objekst
+        /// </summary>
+        /// <param name="data">Das HostData Objekt, zu dem die Verbindung aufgebaut werden soll</param>
         void Connect(HostData data);
 
+        /// <summary>
+        /// Trennt die Verbindung zu dem Remotehost, und benarchtigt diesen über den Verbungsabbau
+        /// </summary>
         void Disconnect();
 
+        /// <summary>
+        /// Beginnt auf eingehende Packete zu lauschen
+        /// </summary>
         void StartListening();
 
+        /// <summary>
+        /// Stoppt auf eigehende Pakete zu lauschen
+        /// </summary>
         void StopListening();
 
+        /// <summary>
+        /// Sendet ein Packet an den Remotehost
+        /// </summary>
+        /// <param name="package">Das zu sendene Packet</param>
         void SendPackage(IPackage package);
 
     }
