2d8f75b661753ebaf7a567f1a384d2aa366b5050
Zakant <zakant@gmx.de>
2014-07-30 16:41:51 +0200

    Dokumentation für "ClientDisconnectedEventArgs" hinzugefügt

--- SimpleNetwork/Events/ClientDisconnectedEventArgs.cs
+++ SimpleNetwork/Events/ClientDisconnectedEventArgs.cs
@@ -6,11 +6,26 @@ using System.Text;
 
 namespace SimpleNetwork.Events
 {
+    /// <summary>
+    /// Stellt Daten für das <see cref="SimpleNetwork.Server.IServer.ClientDisconnected"/> Ereigniss bereit.
+    /// </summary>
     public class ClientDisconnectedEventArgs : SimpleNetworkEventArgs
     {
+        /// <summary>
+        /// Der Grund für den Verbindungsabbruch mit dem Remotehost.
+        /// </summary>
         public DisconnectReason Reason { get; protected set; }
+        
+        /// <summary>
+        /// Der Remotehost.
+        /// </summary>
         public IClient Client { get; protected set; }
 
+        /// <summary>
+        /// Initialisiert eine neue Instanz der HostFoundEventArgs unter verwendung des angegeben HostData-Objekts und des Grundes .
+        /// </summary>
+        /// <param name="c">Der Remotehost</param>
+        /// <param name="r">Der Grund</param>
         public ClientDisconnectedEventArgs(IClient c, DisconnectReason r)
         {
             Client = c;
