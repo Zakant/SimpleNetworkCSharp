6211f1dc806247fe7c5185f788e481df0ca772ca
Zakant <zakant@gmx.de>
2014-07-30 18:05:11 +0200

    Dokumentation für "PackageProvider" teilweise hinzugefügt

--- SimpleNetwork/Package/Provider/PackageProvider.cs
+++ SimpleNetwork/Package/Provider/PackageProvider.cs
@@ -9,11 +9,23 @@ using System.Text;
 
 namespace SimpleNetwork.Package.Provider
 {
+    /// <summary>
+    /// Stellt eine Abstrakte Basisklasse bereit, um Packete an <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" /> weiterzuleiten.
+    /// </summary>
     public abstract class PackageProvider : IPackageProvider
     {
+
+        /// <summary>
+        /// Tritt ein, wenn eine neue Nachricht eintrifft.
+        /// </summary>
         public event EventHandler<Events.NewMessageEventArgs> NewMessage;
 
-
+        /// <summary>
+        /// Löst das <see cref="PackageProvider.NewMessage" /> Ereignis aus.
+        /// </summary>
+        /// <param name="package">Das neu empfangene Packet.</param>
+        /// <param name="c">Der Remotehost, der das Packet versendet hat.</param>
+        /// <returns>Die verwendeten Erignis Argumente.</returns>
         protected NewMessageEventArgs RaiseNewMessage(IPackage package, IClient c)
         {
             var myevent = NewMessage;
@@ -23,7 +35,11 @@ namespace SimpleNetwork.Package.Provider
             return args;
         }
 
-
+        /// <summary>
+        /// Informiert alle <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" />, die den entsprechenden Typ abboniert haben.
+        /// </summary>
+        /// <param name="package">Das Packet, das empfangen wurde.</param>
+        /// <param name="client">Der RemoteHost, der das Packet versendet hat.</param>
         protected void informListener(IPackage package, IClient client)
         {
             if (_listener.ContainsKey(package.GetType()))
@@ -35,6 +51,11 @@ namespace SimpleNetwork.Package.Provider
 
         private Dictionary<Type, IList<object>> _listener = new Dictionary<Type, IList<object>>();
 
+        /// <summary>
+        /// Registriert einen neuen <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" />.
+        /// </summary>
+        /// <typeparam name="T">Den Packettype, den der <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" /> abbonieren möchte.</typeparam>
+        /// <param name="packagelistener">Der <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" />.</param>
         public void RegisterPackageListener<T>(IPackageListener<T> packagelistener) where T : IPackage
         {
             if (!_listener.ContainsKey(typeof(T)))
@@ -48,6 +69,11 @@ namespace SimpleNetwork.Package.Provider
             RegisterPackageListener<T>(new LambdaPackageListener<T>(action));
         }
 
+        /// <summary>
+        /// Entfernt einen <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" />.
+        /// </summary>
+        /// <typeparam name="T">Der Packettype, den <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" /> abboniert hat.</typeparam>
+        /// <param name="packagelistener">Der <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" />, der entfernt werden soll.</param>
         public void RemovePackageListener<T>(IPackageListener<T> packagelistener) where T : IPackage
         {
             if (!_listener.ContainsKey(typeof(T))) return;
