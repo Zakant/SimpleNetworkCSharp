713951abe36133f9a684347527005888fa8ed957
Zakant <zakant@gmx.de>
2014-07-30 17:17:50 +0200

    Dokumentation für "IPackageListener" hinzugefügt

--- SimpleNetwork/Package/Listener/IPackageListener.cs
+++ SimpleNetwork/Package/Listener/IPackageListener.cs
@@ -7,8 +7,17 @@ using System.Text;
 
 namespace SimpleNetwork.Package.Listener
 {
+    /// <summary>
+    /// Stellt Methoden bereit um auf eigehende Packete reagieren zu können.
+    /// </summary>
+    /// <typeparam name="T">Der Typ der Pakete, auf die reagiert werden soll.</typeparam>
     public interface IPackageListener<T> where T : IPackage
     {
+        /// <summary>
+        /// Verarbeitet ein eingehendes Paket.
+        /// </summary>
+        /// <param name="package">Das eigehende Packet.</param>
+        /// <param name="client">Der Remotehost, von dem das Packet stammt.</param>
         void ProcessIncommingPackage(T package, IClient client);
     }
 }
