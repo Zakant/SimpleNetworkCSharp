117dd0e86d38d77ff0265d8c8946af2707e47e6f
Zakant <zakant@gmx.de>
2014-07-30 16:46:57 +0200

    Dokumentation für "DisconnectReason" hinzugefügt

--- SimpleNetwork/Events/DisconnectReasons.cs
+++ SimpleNetwork/Events/DisconnectReasons.cs
@@ -5,12 +5,30 @@ using System.Text;
 
 namespace SimpleNetwork.Events
 {
+    /// <summary>
+    /// Gibt die Gründe für einen Verbindugsabbruch an.
+    /// </summary>
     public enum DisconnectReason : byte
     {
+        /// <summary>
+        /// Verbindung wurde unerwartet geschlossen. Lese- oder Schreibvorgang ist fehlgeschlagen.
+        /// </summary>
         Failed,
+        /// <summary>
+        /// Keine weiteren Information über die Gründe verfügbar.
+        /// </summary>
         Unknown,
+        /// <summary>
+        /// Verbindung geschlossen.
+        /// </summary>
         ConnectionShoutDown,
+        /// <summary>
+        /// Verbindung wurde ortnungsgemäß beendet.
+        /// </summary>
         ClosedProperly,
+        /// <summary>
+        /// Netzwerkverbindung wurde verloren.
+        /// </summary>
         LostConnection
 
     }
