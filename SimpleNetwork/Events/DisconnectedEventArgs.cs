31b6c7849fc477d2ab9edf6a7945dd97809255a6
Zakant <zakant@gmx.de>
2014-07-30 16:44:13 +0200

    Dokumentation für "DisconnectedEventArgs" hinzugefügt

--- SimpleNetwork/Events/DisconnectedEventArgs.cs
+++ SimpleNetwork/Events/DisconnectedEventArgs.cs
@@ -5,10 +5,20 @@ using System.Text;
 
 namespace SimpleNetwork.Events
 {
+    /// <summary>
+    /// Stellt Daten für das <see cref="SimpleNetwork.Client.IClient.Disconnected"/> Ereigniss bereit.
+    /// </summary>
     public class DisconnectedEventArgs : SimpleNetworkEventArgs
     {
+        /// <summary>
+        /// Der Grund für den Verbindungsabbruch.
+        /// </summary>
         public DisconnectReason Reason { get; protected set; }
 
+        /// <summary>
+        /// Initialisiert eine neue Instanz der HostFoundEventArgs unter verwendung des angegeben Grundes .
+        /// </summary>
+        /// <param name="r">Der Grund.</param>
         public DisconnectedEventArgs(DisconnectReason r)
         {
             Reason = r;
