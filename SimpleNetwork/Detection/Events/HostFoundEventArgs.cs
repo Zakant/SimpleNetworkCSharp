4b44c2604e9cdba9b57fde6e976afe852da0241a
Zakant <zakant@gmx.de>
2014-07-30 16:35:44 +0200

    Rechtschreibkorrektur

--- SimpleNetwork/Detection/Events/HostFoundEventArgs.cs
+++ SimpleNetwork/Detection/Events/HostFoundEventArgs.cs
@@ -20,7 +20,7 @@ namespace SimpleNetwork.Detection.Events
         /// <summary>
         /// Initialisiert eine neue Instanz der HostFoundEventArgs unter verwendung des angegeben HostData-Objekts.
         /// </summary>
-        /// <param name="newData">Die Daten des neugefundenen Hosts</param>
+        /// <param name="newData">Die Daten des neugefundenen Hosts.</param>
         public HostFoundEventArgs(HostData newData)
         {
             NewData = newData;
