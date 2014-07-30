666cb23c1ce322323189acaf1e963c75fb43808b
Zakant <zakant@gmx.de>
2014-07-30 18:06:22 +0200

    Interface der Klasse angepasst und Methodensignaturen übernommen

--- SimpleNetwork/Package/Provider/IPackageProvider.cs
+++ SimpleNetwork/Package/Provider/IPackageProvider.cs
@@ -1,4 +1,5 @@
-﻿using SimpleNetwork.Events;
+﻿using SimpleNetwork.Client;
+using SimpleNetwork.Events;
 using SimpleNetwork.Package.Listener;
 using SimpleNetwork.Package.Packages;
 using System;
@@ -26,10 +27,23 @@ namespace SimpleNetwork.Package.Provider
         void RegisterPackageListener<T>(IPackageListener<T> packagelistener) where T : IPackage;
 
         /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="action"></param>
+        public void RegisterPackageListener<T>(Action<T, IClient> action) where T : IPackage;
+
+        /// <summary>
         /// Entfernt einen <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" />.
         /// </summary>
         /// <typeparam name="T">Der Packettype, den <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" /> abboniert hat.</typeparam>
         /// <param name="packagelistener">Der <see cref="SimpleNetwork.Package.Listener.IPackageListener{T}" />, der entfernt werden soll.</param>
         void RemovePackageListener<T>(IPackageListener<T> packagelistener) where T : IPackage;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        void RemoveTypeListener<T>() where T : IPackage;
     }
 }
