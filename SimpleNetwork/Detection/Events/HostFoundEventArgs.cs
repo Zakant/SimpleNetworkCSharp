fe12f23926e1ff28acc72f0748ce35b9c5a26fe9
Zakant <zakant@gmx.de>
2014-07-30 16:33:54 +0200

    Dokumentation für "HostFoundEventArgs" hinzugefügt

--- SimpleNetwork/Detection/Events/HostFoundEventArgs.cs
+++ SimpleNetwork/Detection/Events/HostFoundEventArgs.cs
@@ -6,11 +6,21 @@ using System.Text;
 
 namespace SimpleNetwork.Detection.Events
 {
+    /// <summary>
+    /// Stellt Daten für das <see cref="SimpleNetwork.Detection.Detector.INetworkDetector.HostFound"/> Ereigniss bereit.
+    /// </summary>
     [Serializable]
     public class HostFoundEventArgs : EventArgs
     {
+        /// <summary>
+        /// Die Daten des neu gefunden Hosts.
+        /// </summary>
         public HostData NewData { get; protected set; }
 
+        /// <summary>
+        /// Initialisiert eine neue Instanz der HostFoundEventArgs unter verwendung des angegeben HostData-Objekts.
+        /// </summary>
+        /// <param name="newData">Die Daten des neugefundenen Hosts</param>
         public HostFoundEventArgs(HostData newData)
         {
             NewData = newData;
