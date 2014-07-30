8a64fb98422cad83ce785e337685bf79cd95cb9b
Zakant <zakant@gmx.de>
2014-07-30 16:35:32 +0200

    Dokumentation für "HostLostEventArgs" hinzugefügt

--- SimpleNetwork/Detection/Events/HostLostEventArgs.cs
+++ SimpleNetwork/Detection/Events/HostLostEventArgs.cs
@@ -6,11 +6,21 @@ using System.Text;
 
 namespace SimpleNetwork.Detection.Events
 {
+    /// <summary>
+    /// Stellt Daten für das <see cref="SimpleNetwork.Detection.Detector.INetworkDetector.HostLost"/> Ereigniss bereit.
+    /// </summary>
     [Serializable]
     public class HostLostEventArgs : EventArgs
     {
+        /// <summary>
+        /// Die Daten des verlorenen Hosts.
+        /// </summary>
         public HostData OldData { get; protected set; }
 
+        /// <summary>
+        /// Initialisiert eine neue Instanz der HostFoundEventArgs unter verwendung des angegeben HostData-Objekts.
+        /// </summary>
+        /// <param name="newData">Die Daten des verlorenen Hosts.</param>
         public HostLostEventArgs(HostData oldData)
         {
             this.OldData = oldData;
