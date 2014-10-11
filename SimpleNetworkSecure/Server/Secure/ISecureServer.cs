873ef32d65f2020e37fbc9c4e1a80f5eaa97db03
Zakant <zakant@gmx.de>
2014-10-11 12:22:48 +0200

    Added base secure interfaces

---
+++ SimpleNetworkSecure/Server/Secure/ISecureServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNetwork.Server.Secure
+{
+    public interface ISecureServer
+    {
+    }
+}
