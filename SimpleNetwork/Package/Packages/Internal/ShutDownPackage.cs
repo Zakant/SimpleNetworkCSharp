de016fa13f163165b4fb7ea809f15d4a4d30c812
Zakant <zakant@gmx.de>
2014-10-11 12:31:35 +0200

    Changed access modifier to 'public'

--- SimpleNetwork/Package/Packages/Internal/ShutDownPackage.cs
+++ SimpleNetwork/Package/Packages/Internal/ShutDownPackage.cs
@@ -7,7 +7,7 @@ using System.Text;
 namespace SimpleNetwork.Package.Packages.Internal
 {
     [Serializable]
-    internal class ShutDownPackage : IPackage
+    public class ShutDownPackage : IPackage
     {
         public string ShutDownMessage { get; set; }
         public DisconnectReason Reason { get; set; }
