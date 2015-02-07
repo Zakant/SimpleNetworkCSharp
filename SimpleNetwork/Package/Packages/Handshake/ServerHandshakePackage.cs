f23ddbdb5ac48a2f90ee11b3cf794294f3329d03
Zakant <zakant@gmx.de>
2015-02-07 13:30:24 +0100

    Added Handshake files

---
+++ SimpleNetwork/Package/Packages/Handshake/ServerHandshakePackage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleNetwork.Package.Packages.Handshake
+{
+    public class ServerHandshakePackage : IHandshakePackage
+    {
+    }
+}
