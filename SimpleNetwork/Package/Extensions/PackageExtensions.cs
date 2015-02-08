c5d05dbd6f1da5618ea0877bf98da11c656d7e59
Zakant <zakant@gmx.de>
2015-02-08 17:07:34 +0100

    Completed doc for SimpleNetwork

--- SimpleNetwork/Package/Extensions/PackageExtensions.cs
+++ SimpleNetwork/Package/Extensions/PackageExtensions.cs
@@ -8,14 +8,27 @@ using System.Text;
 
 namespace SimpleNetwork.Package
 {
+    /// <summary>
+    /// Stellt Erweiterungs Methoden fuer Pakete bereit.
+    /// </summary>
     public static class PackageExtensions
     {
+        /// <summary>
+        /// Erstellt eine Kopie eines Paketes.
+        /// </summary>
+        /// <param name="package">Das zu kopierende Paket.</param>
+        /// <returns>Die Kopie des urspruenglichen Paketes.</returns>
         public static IPackage Clone(this IPackage package)
         {
             if (package is ICloneable) return (package as ICloneable).Clone() as IPackage;
             return package.toByteArray().toPackage();
         }
 
+        /// <summary>
+        /// Wandelt ein Paket in ein Byte Array um.
+        /// </summary>
+        /// <param name="package">Das zu behandelnde Paket.</param>
+        /// <returns>Die Byte Array Repraesentation des Paketes.</returns>
         public static byte[] toByteArray(this IPackage package)
         {
             using (MemoryStream ms = new MemoryStream())
@@ -26,6 +39,11 @@ namespace SimpleNetwork.Package
             }
         }
 
+        /// <summary>
+        /// Wandelt ein Byte Array in ein Paket um.
+        /// </summary>
+        /// <param name="data">Das Byte Array aus dem das Paket gelesen werden soll.</param>
+        /// <returns>Das gelesene Paket.</returns>
         public static IPackage toPackage(this byte[] data)
         {
             using (MemoryStream ms = new MemoryStream(data))
